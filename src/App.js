4addb188c2f131862517daa46dba166b1af52253
Fahmi <fahmiali130499@gmail.com>
2022-12-03 02:59:36 +0700

    add foldering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Main from "./components/pages/main";
+import Navbar from "./components/navigation/";
+import Footer from "./components/footer/";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Route exact path="/" component={Main} />
+        <Footer />
+      </Router>
     </div>
   );
 }
