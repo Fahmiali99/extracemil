3f8090b42037b4b8b0fbe8d65aca798d7c0a9a29
Rahmi Azizah <79822759+rahmi-na@users.noreply.github.com>
2022-12-14 02:42:00 +0700

    add some feature

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./components/pages/main";
-import Navbar from "./components/navigation/";
-import Footer from "./components/footer/";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
         <Route exact path="/" component={Main} />
-        <Footer />
       </Router>
     </div>
   );
--- src/components/pages/profile/index.js
+++ src/components/pages/profile/index.js
@@ -1,7 +1,45 @@
 import React from "react";
+import { data } from "../../../data/data";
 
 function index() {
-  return <div>Profile</div>;
+  return (
+    // <div>Profile
+    // </div>
+    <div>
+      <div className="avatar w-full flex justify-center">
+        <div className="w-24 rounded-full">
+          <img src="https://placeimg.com/192/192/people" />
+        </div>
+      </div>
+      <div className=" text-center">
+        <p className=" pt-10 font-medium text-xl">Extra Cemil</p>
+        <p className=" pt-4 font-extralight text-sm">
+          Malang, Jawa Timur, Indonesia
+        </p>
+      </div>
+
+      <div className=" pt-10  ">
+        {data.map((item, index) => (
+          <div key={index}>
+            <a href={item.url}>
+              <div className="pb-5">
+                <button className=" btn w-96 md:w-80 normal-case">
+                  <div className=" grid grid-cols-2 items-center">
+                    <div className=" ">
+                      <img src={item.icon} className="  w-8 " />
+                    </div>
+                    <div>
+                      <h1 className="">{item.name}</h1>
+                    </div>
+                  </div>
+                </button>
+              </div>
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default index;
